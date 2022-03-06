1a5b32ddb531e2d408b415d1554d1509aba9b68e
Bruno-Yu <jackhellowin@gmail.com>
2022-03-06 18:22:38 +0800

    vue.config revised

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = {
-  publicPath: './',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/bruno-yu.github.io/week-6-project/'
+      : '/',
 };
